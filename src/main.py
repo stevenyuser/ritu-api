f83a8592bf5ef06ba07c728a553f9bc7f59a716c
Steven Yu <stevenyuser@gmail.com>
2023-10-21 16:40:36 -0400

    added initial tests for sms method for twilio

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Response
 from src.firebase.auth import start_firebase
+
+from twilio import twiml
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = FastAPI()
 db = start_firebase()
@@ -12,3 +15,10 @@ async def ping():
 @app.get("/paul", status_code=200)
 async def paul():
     return {"message": db.collection('users').document('1').get().to_dict()}
+
+
+@app.post("/sms")
+async def sms(From: str = Form(...), Body: str = Form(...)):
+    response = MessagingResponse()
+    msg = response.message(f"Hi {From}, you said: {Body}")
+    return Response(content=str(response), media_type="application/xml")
